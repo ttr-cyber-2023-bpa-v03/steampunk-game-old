bf67bb216a98ca98e3dc3cac622aeaf6ab409c72
re1oop <reversed-coffee@proton.me>
2023-11-10 17:20:06 -0600

    Push partial

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,22 @@ import "@styles/index.scss";
 import { Camera, Renderer } from "@logic/graphics";
 
 const canvas = document.getElementById("frame") as HTMLCanvasElement;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 const renderer = new Renderer();
 renderer.init(canvas).then(ctx => {
     const camera = new Camera(ctx);
 
+    const o: any = {
+        hue: 0
+    };
+
     // TODO: we need a task scheduler for this
     setInterval(() => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        camera.render();
+
+        o.hue += 0.1;
+
+        camera.render(o);
     }, 0);
 });
--- src/logic/graphics/Camera.ts
+++ src/logic/graphics/Camera.ts
@@ -17,11 +17,12 @@ export class Camera {
         this.context = ctx;   
     }
 
-    public render() {
+    public render(o: any) {
         // import gl, canvas
         const gl = this.context.gl;
         const canvas = this.context.canvas;
-        
+        const program = this.context.program;
+
         // Set the resolution
         gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
         gl.uniform2f(this.glResolution, canvas.clientWidth, canvas.clientHeight);
@@ -29,21 +30,6 @@ export class Camera {
         // Clear the screen
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
-
-        // Draw red square from 10,10 to 100,100
-        // We're going to 100% need some abstraction for matrices
-        const buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            10, 10,
-            100, 10,
-            10, 100,
-            10, 100,
-            100, 10,
-            100, 100
-        ]), gl.STATIC_DRAW);
-        gl.enableVertexAttribArray(this.glPosition);
-        gl.vertexAttribPointer(this.glPosition, 2, gl.FLOAT, false, 0, 0);
 
         // Draw the rectangle
         gl.drawArrays(gl.TRIANGLES, 0, 6);
