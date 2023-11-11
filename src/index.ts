609ed153be52a77ef3470be8b6143176307a05c2
re1oop <reversed-coffee@proton.me>
2023-11-11 01:49:04 -0600

    Test run w/ mouse

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import "@styles/index.scss";
 
-import { Scheduler } from "@logic/scheduler";
-
-import { Renderer } from "@logic/graphics";
-import { JobOperation } from "@logic/scheduler/Job";
-
-const canvas = document.getElementById("frame") as HTMLCanvasElement;
+import { JobOperation, Scheduler } from "@logic/scheduler";
+import { Scene } from "@logic/graphics";
+import { Rectangle } from "@logic/graphics/objects/Rectangle";
 
 const scheduler = new Scheduler();
 scheduler.start();
 
-scheduler.schedule(Scheduler.createJob("init", async () => {
-    console.log("running");
-    return JobOperation.Abort;
-}));
+const scene = new Scene();
+scene.init(document.getElementById("frame") as HTMLCanvasElement).then(async () => {
+    const rect = new Rectangle(scene);
+    rect.visible = true;
+    rect.position = [10, 10];
+    rect.size = [100, 100];
+    await rect.allocate();
+
+    scene.objects.push(rect);
+    scheduler.schedule(Scheduler.createJob("render", async () => {
+        await scene.render();
+        return JobOperation.Resume;
+    }));
+
+    // window mouse event
+    window.addEventListener("mousemove", (ev) => {
+        rect.position = [ev.clientX, ev.clientY];
+        rect.update();
+    });
+});
